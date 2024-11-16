b94309a336082346126751db7b7946fa1342c0af
Sepiro10 <126035782+Sepiro10@users.noreply.github.com>
2024-11-15 22:12:03 -0500

    Delete Front directory

--- Front/src/components/PrivateRoute/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import { isAuthenticated } from '../../services/authService';
-
-const PrivateRoute = () => {
-    return isAuthenticated() ? <Outlet /> : <Navigate to="/login" />;
-  };
-  
-  export default PrivateRoute;
--- Front/src/services/authService.js
+++
@@ -1,5 +0,0 @@
-const isAuthenticated = () => {
-  const token = localStorage.getItem("token"); // O sessionStorage
-  return token !== null;
-};
-  
